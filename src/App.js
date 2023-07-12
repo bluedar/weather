2191cdf734b7725313201b140de1f381634ad838
bluedear <128768429+bluedar@users.noreply.github.com>
2023-07-12 17:10:29 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+
 import WeatherBox from "./components/WeatherBox";
 import WeatherButton from "./components/WeatherButton";
-import FadeLoader from "react-spinners/FadeLoader";
+import RingLoader from "react-spinners/RingLoader";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   const [weather, setWeather] = useState(null); //날씨데이타
+  const cities = ["Dubai", "Tehran", "Tokyo", "Seoul"];
   const [city, setCity] = useState("current");
   const [loading, setLoading] = useState(true);
-  const cities = ["sydney", "oslo", "baghdad", "paris"];
 
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
       let lon = position.coords.longitude;
-      //console.log('현재 내 위치는?',lat,lon)
+      //console.log("현재 내 위치는?", lat, lon);
 
       getWeatherByCurrentLocation(lat, lon);
     });
   };
-
-  //좌표를 이용해서 날씨API가져오기
+  //좌표를 이용해서 날씨 API가져오기
   const getWeatherByCurrentLocation = async (lat, lon) => {
     setLoading(true);
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=83aeb80a15d50ee7a248d29575f07e8d&units=metric`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=0d17f74ec0e3138fe53cff48e29df572&units=metric`;
     let response = await fetch(url);
     let data = await response.json();
     setWeather(data);
     setLoading(false);
   };
-
-  //도시이름으로 날씨API가져오기
+  //도시이름으로 날씨 API가져오기
   const getWeatherByCity = async () => {
     setLoading(true);
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=83aeb80a15d50ee7a248d29575f07e8d&units=metric`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0d17f74ec0e3138fe53cff48e29df572&units=metric`;
     let response = await fetch(url);
     let data = await response.json();
     setWeather(data);
@@ -53,14 +52,7 @@ function App() {
     <>
       {loading ? (
         <div className="container">
-          <FadeLoader
-            color="#0d6efd"
-            height={40}
-            margin={30}
-            radius={10}
-            speedMultiplier={2}
-            width={6}
-          />
+          <RingLoader color="cyan" size={120} speedMultiplier={1} />
         </div>
       ) : (
         <div className="container">
